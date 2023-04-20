87ecc56fa9e4f1f1eba73787066cf1e892e1a301
DannyGleeson <131392723+DannyGleeson@users.noreply.github.com>
2023-04-20 22:08:17 +0100

    Update cam-box.py

--- cam-box.py
+++ cam-box.py
@@ -21,20 +21,20 @@ while True:
 
     # lower and upper bounds for green color
     lower_green = np.array([36, 25, 25])
-    upper_green = np.array([70, 255,255]))
+    upper_green = np.array([70, 255,255])
     mask_green = cv2.inRange(hsv, lower_green, upper_green)
 
     # lower and upper bounds for blue color
-    lower_blue = np.array([50, 0, 50])
-    upper_blue = np.array([255,180,255])
+    lower_blue = np.array([50, 50, 0])
+    upper_blue = np.array([255,255,180])
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    mask = cv2.bitwise_or(mask_red, cv2.bitwise_or(mask_green, mask_blue))
+    mask = cv2.bitwise_or(mask_blue, cv2.bitwise_or(mask_green, mask_red))
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 10000:
+        if area > 9000:
             x, y, w, h = cv2.boundingRect(contour)
             color = (0, 0, 0)
             if mask_red[y:y+h, x:x+w].any():
@@ -48,7 +48,7 @@ while True:
                 cv2.putText(frame, "Blue Smartie", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Color Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
