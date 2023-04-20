041bf8882c2bbbc8e5b2bfa01272fb20703f57c6
DannyGleeson <131392723+DannyGleeson@users.noreply.github.com>
2023-04-20 22:37:11 +0100

    Update cam-box.py

--- cam-box.py
+++ cam-box.py
@@ -29,23 +29,27 @@ while True:
     upper_blue = np.array([255,255,180])
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
 
+    # Mergers all maska together into one mask  
     mask = cv2.bitwise_or(mask_blue, cv2.bitwise_or(mask_green, mask_red))
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # contours are the shapes that the camera recognises as one object on screen, 
+    # it uses the mask variable as what colors it needs to detect
+    # cv2.RETR_TREE : retrevial in tree mode, retrieves all objects(smarties) and organises them into a heiharchy , parent child relationship
+    smarties, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for contour in contours:
-        area = cv2.contourArea(contour)
+    for smartie in smarties:
+        area = cv2.contourArea(smartie)
         if area > 9000:
-            x, y, w, h = cv2.boundingRect(contour)
+            x, y, w, h = cv2.boundingRect(smartie)
             color = (0, 0, 0)
             if mask_red[y:y+h, x:x+w].any():
                 color = (0, 0, 255) # red color
-                cv2.putText(frame, "Red Smartie", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                cv2.putText(frame, "Red Smartie", (x, y), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
             elif mask_green[y:y+h, x:x+w].any():
                 color = (0, 255, 0) # green color
-                cv2.putText(frame, "Green Smartie", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                cv2.putText(frame, "Green Smartie", (x, y), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
             elif mask_blue[y:y+h, x:x+w].any():
                 color = (255, 0, 0) # blue color
-                cv2.putText(frame, "Blue Smartie", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                cv2.putText(frame, "Blue Smartie", (x, y), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
     cv2.imshow("Color Detection", frame)
